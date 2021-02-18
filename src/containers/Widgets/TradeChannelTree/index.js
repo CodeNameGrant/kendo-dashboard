ef85ae80ee4155710be598e78e56067eff290c5e
GrantW <Grant.Walker@ksdc.co.za>
2021-02-18 12:17:03 +0200

    Created Trade Channel Tree Component

--- src/containers/Widgets/TradeChannelTree/index.js
+++ src/containers/Widgets/TradeChannelTree/index.js
@@ -1,12 +1,111 @@
 import React from 'react'
+import { TreeView } from '@progress/kendo-react-treeview'
+import { Button } from '@progress/kendo-react-buttons'
 
-export default function TradeChannelTree({ }) {
+import classes from './TradeChannelTree.module.css';
 
+export default function TradeChannelTree({
+  tradeChannels,
+  setTradeChannels,
+  selection = 'single',
+  onSelect,
+  width,
+  height
+}) {
 
+  const onExpandChangeHandler = ({ item }) => {
+    const node = findNodeById(tradeChannels, item.id);
+    if (node) {
+      node.expanded = !node.expanded;
+    }
+  }
+
+  const onItemClickHandler = ({ item }) => {
+    if (selection === 'single') {
+      deselectAllNodes(tradeChannels);
+    }
+
+    const node = findNodeById(tradeChannels, item.id);
+    if (node) {
+      node.selected = true;
+    }
+
+    if (onSelect) {
+      onSelect(item);
+    }
+  }
+
+  const toggleExpandAllHandler = (expandAll) => {
+    setTradeChannels(prevState => mofityTreeNodes(prevState, (item) => item.expanded = expandAll))
+  }
+
+  const clearSelectionHandler = () => {
+    setTradeChannels(prevState => mofityTreeNodes(prevState, (item) => item.selected = false))
+    onSelect(null);
+  }
 
   return (
-    <React.Fragment>
-      Test
-    </React.Fragment>
+    <div className={classes.Container} style={{ width, height }}>
+      <div className={classes.ActionBar}>
+        <Button look={'flat'} icon={'plus-outline'} title={'Expand All'} onClick={() => toggleExpandAllHandler(true)}></Button>
+        <Button look={'flat'} icon={'minus-outline'} title={'Collapse All'} onClick={() => toggleExpandAllHandler(false)}></Button>
+        <Button look={'flat'} icon={'filter-clear'} title={'Clear Selection'} onClick={clearSelectionHandler}></Button>
+      </div>
+      <TreeView
+        data={tradeChannels}
+        textField='text'
+        expandIcons={true}
+        onExpandChange={onExpandChangeHandler}
+        onItemClick={onItemClickHandler} />
+    </div>
   )
 }
+
+
+
+// UTILS
+
+const mofityTreeNodes = (treeNodes, modify) => {
+  const nodeArray = [...treeNodes];
+
+  for (let i = 0; i < nodeArray.length; i++) {
+    modify(nodeArray[i]);
+
+    if (nodeArray[i].items) {
+      mofityTreeNodes(nodeArray[i].items, modify);
+    }
+  }
+
+  return nodeArray;
+};
+
+export const findNode = (array, field, value) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][field] && array[i][field] === value) {
+      return array[i];
+    }
+    if (array[i].items) {
+      let result = findNode(array[i].items, field, value);
+      if (result) {
+        return result;
+      }
+    }
+  }
+
+  return null;
+};
+
+export const findNodeById = (array, id) => {
+  return findNode(array, "id", id);
+};
+
+export const deselectAllNodes = (tree) => {
+  for (let i = 0; i < tree.length; i++) {
+    tree[i].selected = false;
+    if (tree[i].items) {
+      deselectAllNodes(tree[i].items);
+    }
+  }
+
+  return tree;
+};
--- src/containers/Widgets/TradeChannelTreeDemo.js
+++ src/containers/Widgets/TradeChannelTreeDemo.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TradeChannelTree from './TradeChannelTree'
+import masterData from './TradeChannelTree/trade-channels.json';
 
 export default function TradeChannelTreeDemo() {
+  const [tradeChannels, setTradeChannels] = useState(masterData);
+
+  const onSelectHandler = (item) => {
+    console.log('onSelectHandler', item);
+  }
+
+  console.log('tradeChannels', tradeChannels);
+
+
   return (
-    <TradeChannelTree />
+    <TradeChannelTree
+      tradeChannels={tradeChannels}
+      setTradeChannels={setTradeChannels}
+      onSelect={onSelectHandler}
+      width={'200px'}
+      height={'300px'} />
   )
 }
+
+
+// Utils
